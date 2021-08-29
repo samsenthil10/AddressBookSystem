5280b36d1e5d0c2f34f7010efd0b839e50b9f8cb
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-30 01:12:55 +0530

    Modified: To manage multiple contacts

--- src/com/bridgelabz/addressbooksystem/AddressBookSystemImpl.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookSystemImpl.java
@@ -1,89 +1,115 @@
 package com.bridgelabz.addressbooksystem;
 
-import java.util.Scanner;
-
 public class AddressBookSystemImpl implements AddressBookSystemIF {
-
-	Scanner scanner = new Scanner(System.in);
-
+	
 	@Override
 	public void createContacts(Contacts contacts) {
 
 		System.out.print("Enter First Name: ");
-		contacts.setFirstName(scanner.nextLine());
+		contacts.setFirstName(AddressBookMain.scanner.nextLine());
 		System.out.print("Enter Last Name: ");
-		contacts.setLastName(scanner.nextLine());
+		contacts.setLastName(AddressBookMain.scanner.nextLine());
 		System.out.print("Enter Address: ");
-		contacts.setAddress(scanner.nextLine());
+		contacts.setAddress(AddressBookMain.scanner.nextLine());
 		System.out.print("Enter City: ");
-		contacts.setCity(scanner.nextLine());
+		contacts.setCity(AddressBookMain.scanner.nextLine());
 		System.out.print("Enter State: ");
-		contacts.setState(scanner.nextLine());
+		contacts.setState(AddressBookMain.scanner.nextLine());
 		System.out.print("Enter Zip Code: ");
-		contacts.setZip(scanner.nextInt());
+		contacts.setZip(AddressBookMain.scanner.nextInt());
 		System.out.print("Enter Phone Number: ");
-		scanner.nextLine();
-		contacts.setPhoneNumber(scanner.nextInt());
-		scanner.nextLine();
+		AddressBookMain.scanner.nextLine();
+		contacts.setPhoneNumber(AddressBookMain.scanner.nextInt());
+		AddressBookMain.scanner.nextLine();
 		System.out.print("Enter E Mail ID: ");
-		contacts.setEmail(scanner.nextLine());
-
+		contacts.setEmail(AddressBookMain.scanner.nextLine());
 	}
 
 	@Override
-	public void editContacts(Contacts contacts) {
+	public void editContacts(Contacts[] contacts, int contactTopPointer, int totalNumberOfContacts) {
 
-		System.out.print("Enter the First Name of contact to be edited: ");
-		String firstName = scanner.nextLine();
-		if(firstName.equalsIgnoreCase(contacts.getFirstName())) {
-			System.out.print("1.First Name\n2.Last Name\n3.Address\n4.City\n5.State\n6.Zip Code\n7.Phone Number\n8.Email ID\nSelect the field to edit: ");
-			int choice = scanner.nextInt();
-			scanner.nextLine();
-			switch(choice) {
-			case 1:System.out.print("Enter First Name: ");
-			contacts.setFirstName(scanner.nextLine());
-			break;
-			case 2:System.out.print("Enter Last Name: ");
-			contacts.setLastName(scanner.nextLine());
-			break;
-			case 3:System.out.print("Enter Address: ");
-			contacts.setAddress(scanner.nextLine());
-			break;
-			case 4:System.out.print("Enter City: ");
-			contacts.setCity(scanner.nextLine());
-			break;
-			case 5:System.out.print("Enter State: ");
-			contacts.setState(scanner.nextLine());
-			break;
-			case 6:System.out.print("Enter Zip Code: ");
-			contacts.setZip(scanner.nextInt());
-			scanner.nextLine();
-			break;
-			case 7:System.out.print("Enter Phone Number: ");
-			contacts.setPhoneNumber(scanner.nextInt());
-			scanner.nextLine();
-			break;
-			case 8:System.out.print("Enter E Mail ID: ");
-			contacts.setEmail(scanner.nextLine());
-			break;
-			default:System.err.println("Invalid Option!");
+		if(contactTopPointer == -1) {
+			System.out.println("Contacts Empty!");
+		}
+		else {
+			System.out.print("Enter the Phone Number of contact to be edited: ");
+			int phoneNumber = AddressBookMain.scanner.nextInt();
+			int editIndex = -1;
+			for(int index = 0; index <= contactTopPointer && contactTopPointer < totalNumberOfContacts ; index++) {
+				if((phoneNumber == contacts[index].getPhoneNumber()))
+					editIndex = index;
+			}
+			if(editIndex == -1) {
+				System.out.println("Contact Not found");
+			}
+			else {
+				System.out.print("1.First Name\n2.Last Name\n3.Address\n4.City\n5.State\n6.Zip Code\n7.Phone Number\n8.Email ID\nSelect the field to edit: ");
+				int choice = AddressBookMain.scanner.nextInt();
+				AddressBookMain.scanner.nextLine();
+				switch(choice) {
+				case 1:System.out.print("Enter First Name: ");
+				contacts[editIndex].setFirstName(AddressBookMain.scanner.nextLine());
+				break;
+				case 2:System.out.print("Enter Last Name: ");
+				contacts[editIndex].setLastName(AddressBookMain.scanner.nextLine());
+				break;
+				case 3:System.out.print("Enter Address: ");
+				contacts[editIndex].setAddress(AddressBookMain.scanner.nextLine());
+				break;
+				case 4:System.out.print("Enter City: ");
+				contacts[editIndex].setCity(AddressBookMain.scanner.nextLine());
+				break;
+				case 5:System.out.print("Enter State: ");
+				contacts[editIndex].setState(AddressBookMain.scanner.nextLine());
+				break;
+				case 6:System.out.print("Enter Zip Code: ");
+				contacts[editIndex].setZip(AddressBookMain.scanner.nextInt());
+				AddressBookMain.scanner.nextLine();
+				break;
+				case 7:System.out.print("Enter Phone Number: ");
+				contacts[editIndex].setPhoneNumber(AddressBookMain.scanner.nextInt());
+				AddressBookMain.scanner.nextLine();
+				break;
+				case 8:System.out.print("Enter E Mail ID: ");
+				contacts[editIndex].setEmail(AddressBookMain.scanner.nextLine());
+				break;
+				default:System.out.println("Invalid Option!");
+				}
 			}
 		}
-		else {
-			System.err.println("Contact Not Found!");
-		}
 	}
 
 	@Override
-	public void deleteContacts(Contacts contacts) {
-		System.out.print("Enter the First Name of contact to be edited: ");
-		String firstName = scanner.nextLine();
-		if(firstName.equalsIgnoreCase(contacts.getFirstName())) {
-			contacts=null;
-			System.out.println("Contact Successfully Deleted");
+	public int deleteContacts(Contacts[] contacts, int contactTopPointer, int totalNumberOfContacts) {
+		if(contactTopPointer == -1) {
+			System.out.println("Contacts Empty!");
 		}
 		else {
-			System.err.println("Contact Not Found!");
+			System.out.print("Enter the Phone Number of contact to be edited: ");
+			int phoneNumber = AddressBookMain.scanner.nextInt();
+			int deleteIndex = -1;
+			for(int index = 0; index <= contactTopPointer && contactTopPointer<totalNumberOfContacts; index++) {
+				if((phoneNumber == contacts[index].getPhoneNumber()))
+					deleteIndex = index;
+			}
+			if(deleteIndex == -1) {
+				System.out.println("Contact Not found");
+			}
+			else {
+				if(deleteIndex == 0) {
+					contacts[deleteIndex]=null;
+					System.out.println("Successfully Deleted Record");
+				    contactTopPointer--;
+				}
+				else {
+					contacts[deleteIndex]=null;
+					for(int index = deleteIndex; index < contactTopPointer; index++)
+						contacts[index]=contacts[index+1];
+					System.out.println("Successfully Deleted Record");
+					contactTopPointer--;
+				}
+			}
 		}
+		return contactTopPointer;
 	}
 }
