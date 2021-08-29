9cf12339f89c13b67193a8ab87ecca10c3db02bc
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-30 01:11:42 +0530

    Modified: The abstract methods to support multiple contacts

--- src/com/bridgelabz/addressbooksystem/AddressBookSystemIF.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookSystemIF.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.addressbooksystem;
 
 public interface AddressBookSystemIF {
-	
+
 	public void createContacts(Contacts contacts);
-	public void editContacts(Contacts contacts);
-	public void deleteContacts(Contacts contacts);
+	public void editContacts(Contacts[] contacts,  int contactTopPointer, int totalNumberOfContacts);
+	public int deleteContacts(Contacts[] contacts,  int contactTopPointer, int totalNumberOfContacts);
 }
