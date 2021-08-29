8f17078531152541fc0049f346a548337dec7916
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-30 04:39:57 +0530

    Added: To create address book with unique name

--- src/com/bridgelabz/addressbooksystem/AddressBookSystemIF.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookSystemIF.java
@@ -5,4 +5,5 @@ public interface AddressBookSystemIF {
 	public void createContacts(Contacts contacts);
 	public void editContacts(Contacts[] contacts,  int contactTopPointer, int totalNumberOfContacts);
 	public int deleteContacts(Contacts[] contacts,  int contactTopPointer, int totalNumberOfContacts);
+	public int createAddressBooks(String[] addressBooksNames, int addressBookCounter, int totalNumberOfAddressBooks);
 }
