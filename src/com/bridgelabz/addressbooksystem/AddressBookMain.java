6711889b20ff3a418d6f76af6e351846d518ad73
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-29 21:47:12 +0530

    Added: To accept person details via console

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -1,9 +1,37 @@
 package com.bridgelabz.addressbooksystem;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+
+	public static void createContacts() {
+
+		Contacts contacts = new Contacts();
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter First Name: ");
+		contacts.setFirstName(scanner.nextLine());
+		System.out.print("Enter Last Name: ");
+		contacts.setLastName(scanner.nextLine());
+		System.out.print("Enter Address: ");
+		contacts.setAddress(scanner.nextLine());
+		System.out.print("Enter City: ");
+		contacts.setCity(scanner.nextLine());
+		System.out.print("Enter State: ");
+		contacts.setState(scanner.nextLine());
+		System.out.print("Enter Zip Code: ");
+		contacts.setZip(scanner.nextInt());
+		System.out.print("Enter Phone Number: ");
+		scanner.nextLine();
+		contacts.setPhoneNumber(scanner.nextInt());
+		scanner.nextLine();
+		System.out.print("Enter E Mail ID: ");
+		contacts.setEmail(scanner.nextLine());
+		scanner.close();
+	}
 
 	public static void main(String[] args) {
 
 		System.out.println("----------Welcome To Address Book Program----------");
+		createContacts();
 	}
 }
