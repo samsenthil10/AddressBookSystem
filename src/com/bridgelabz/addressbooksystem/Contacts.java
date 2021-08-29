06be14fb1735cdf492f1e069ea446fbf46f16074
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-29 21:22:14 +0530

    Added: To enable ability to create a new contact

---
+++ src/com/bridgelabz/addressbooksystem/Contacts.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbooksystem;
+
+public class Contacts {
+	
+	String firstName;
+	String lastName;
+	String address;
+	String city;
+	String state;
+	int zip;
+	int phoneNumber;
+	String email;
+}
